767085c307b2a7b905dde6ef5f221db86b6cec57
baizhizhen <bzz_xd@sina.com>
2018-12-24 10:33:07 +0800

    删除冗余代码

--- src/main/java/com/ibai/patterns/observer/WeatherData.java
+++ src/main/java/com/ibai/patterns/observer/WeatherData.java
@@ -12,30 +12,6 @@ public class WeatherData implements Observerable {
     private float pressure;
     private Set<Observer> observers = new HashSet<>();
 
-    public float getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(float temperature) {
-        this.temperature = temperature;
-    }
-
-    public float getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(float humidity) {
-        this.humidity = humidity;
-    }
-
-    public float getPressure() {
-        return pressure;
-    }
-
-    public void setPressure(float pressure) {
-        this.pressure = pressure;
-    }
-
     public void measurementsChanged() {
         this.notifyObservers();
     }
